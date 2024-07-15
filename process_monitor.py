3c0215c1a1ceb66eec5775ca41801ef27a4deb35
Mark Kochanenko <mkmarkusstvs@gmail.com>
2024-07-15 04:50:12 +0300

    Changes in process_monitor.py: added logging and requests. Changed config file, added requirements.txt

--- process_monitor.py
+++ process_monitor.py
@@ -1,26 +1,30 @@
 import configparser
 import sqlite3
 import psutil
+import logging
 import time
-from datetime import datetime
 import os
+import requests
 
-# Чтение конфигурации
+from datetime import datetime
+
+
+logging.basicConfig(filename='pmonitor.log', level=logging.ERROR, format='%(asctime)s %(levelname)s:%(message)s')
+
 config = configparser.ConfigParser()
-config.read('config.ini')
+config.read('pmonitor.conf')
 
 prefix = config['Settings']['prefix']
 poll_interval = int(config['Settings']['poll_interval'])
 db_path = config['Settings']['db_path']
 exclude_processes = config['Filters']['exclude_processes'].split()
+server_url = config['Settings']['server_url']
 
-# Создание базы данных
 db_name = f"{prefix}_{datetime.now().strftime('%Y%m%d')}.db"
 db_full_path = os.path.join(db_path, db_name)
 conn = sqlite3.connect(db_full_path)
 cursor = conn.cursor()
 
-# Создание таблицы
 cursor.execute('''
 CREATE TABLE IF NOT EXISTS pmonitor (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -44,6 +48,19 @@ def get_active_processes():
     return processes
 
 
+def sync_with_server():
+    try:
+        cursor.execute('SELECT id, datetime, processname, username FROM pmonitor WHERE synced = 0')
+        unsynced_data = cursor.fetchall()
+        if unsynced_data:
+            response = requests.post(server_url, json=unsynced_data)
+            if response.status_code == 200:
+                cursor.execute('UPDATE pmonitor SET synced = 1 WHERE synced = 0')
+                conn.commit()
+    except requests.exceptions.RequestException as exc:
+        logging.error(f'Failed to sync with server: {exc}')
+
+
 def main():
     while True:
         processes = get_active_processes()
@@ -56,6 +73,7 @@ def main():
             ''', (current_time, proc['name'], proc['username']))
 
         conn.commit()
+        sync_with_server()
         time.sleep(poll_interval)
 
 
